e9c5ebe68262037dc018dff8a8f822b35242af18
IshidaMotohiro <ishida.motohiro@tokushima-u.ac.jp>
2015-11-06 14:45:46 +0900

    second

--- ggplot2.R
+++ ggplot2.R
@@ -1,7 +1,17 @@
 
 
 ggplot(iris, aes(x = Species, y = Sepal.Width)) +
-  stat_summary(fun.y = mean, geom = "bar")
+    stat_summary(fun.y = mean, geom = "bar")
 
-ggplot(iris, aes(x = reorder(x = Species, X = Sepal.Width, FUN = mean), y = Sepal.Width)) +  stat_summary(fun.y = mean, geom = "bar") + xlab( "mean")
-x
+ggplot(iris, aes(x = Species, y = Sepal.Width)) +
+    stat_summary(fun.y = mean, geom = "bar") + scale_x_discrete(limits = c("versicolor","virginica"))#,"setosa"))
+# + scale_x_discrete(limits = c("versicolor","virginica","setosa"))
+
+ggplot(iris, aes(x = reorder(x = Species, X = Sepal.Width, FUN = mean), y = Sepal.Width)) +  stat_summary(fun.y = sum, geom = "bar") + xlab( "sum")
+
+
+
+df <- data.frame(x1 = c(30,20,30,10,50,60),x2 = c("AA", "BB", "CC", "DD", "EE", "FF")) ;df
+
+ggplot(df, aes (x = x2, y = x1)) + geom_bar(stat="identity") + scale_x_discrete(limits = c("FF","EE", "CC", "AA"))#, "BB", "DD")) #("bar")
+
